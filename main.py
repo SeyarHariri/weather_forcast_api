062a02be99315a1e5b957c3e487fe0e807727ee7
Hariri <seyarserathariri@gmail.com>
2023-02-27 18:40:11 -0500

    Add weather Api and filter the data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 st.title("Weather forcast for the next days")
 place = st.text_input("Place: ")
 day = st.slider("Forcast Days: ", min_value=1, max_value=5,
@@ -7,14 +8,12 @@ day = st.slider("Forcast Days: ", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {day} days in {place}.")
-def get_day(day):
-    date = ["2023-02-24", "2023-02-25", "2023-03-26"]
-    temperature = [10, 11, 15]
-    temperature = [day * i for i in temperature]
-    return date, temperature
-
-d, t = get_day(day)
 
 
-figure = px.line(x=d, y=t, labels={"x": "date", "y": "temperature (C)"} )
+data = get_data(place, day, option)
+
+
+
+figure = px.line(x=d, y=t, labels={"x": "date", "y": "temperature (C)"})
+
 st.plotly_chart(figure)
