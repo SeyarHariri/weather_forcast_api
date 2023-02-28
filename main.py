8e82905e3e7a04a2bba36b8392e03f44a808f154
Hariri <seyarserathariri@gmail.com>
2023-02-27 19:22:30 -0500

    Finish the project

--- main.py
+++ main.py
@@ -9,11 +9,20 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {day} days in {place}.")
 
+if place:
+    filtered_data = get_data(place, day)
 
-data = get_data(place, day, option)
+    if option == "Temperature":
+        temprature = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temprature, labels={"x": "date", "y": "temperature (C)"})
+        st.plotly_chart(figure)
 
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/clear.png"}
+        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+        print(sky_condition)
+        image_path = [images[condition] for condition in sky_condition]
 
-
-figure = px.line(x=d, y=t, labels={"x": "date", "y": "temperature (C)"})
-
-st.plotly_chart(figure)
+        st.image(image_path, width=115)
